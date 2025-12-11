7833d65dd6529af7797f710831c534e1424e23d1
Carlos Camilo <carlos.fernandezmadrazo@externos-cl.cencosud.com>
2025-12-11 12:54:03 -0500

    feat: add HTTPS development server and script to run it for Next.js.

---
+++ server.mjs
@@ -0,0 +1,33 @@
+import { createServer } from 'https';
+import { parse } from 'url';
+import next from 'next';
+import fs from 'fs';
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = '0.0.0.0'; // Listen on all network interfaces
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+    key: fs.readFileSync('./.cert/localhost-key.pem'),
+    cert: fs.readFileSync('./.cert/localhost.pem'),
+};
+
+app.prepare().then(() => {
+    createServer(httpsOptions, async (req, res) => {
+        try {
+            const parsedUrl = parse(req.url, true);
+            await handle(req, res, parsedUrl);
+        } catch (err) {
+            console.error('Error occurred handling', req.url, err);
+            res.statusCode = 500;
+            res.end('internal server error');
+        }
+    }).listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on https://localhost:${port}`);
+        console.log(`> Also available on https://192.168.1.103:${port}`);
+    });
+});
